cf98275cb8ef827b9c8141e8f7eaf9602d17dc96
Halx Hernandez <halxhernandez@gmail.com>
2025-10-14 15:39:58 -0600

    Ejercicios Dia 17

---
+++ 17-Dia-17-Manejo-De-Exepciones.py
@@ -0,0 +1,11 @@
+# Ejercicios Dia 17
+
+# 1. names = ["Finland", "Sweden", "Norway","Denmark","Iceland", "Estonia","Russia"]. Descomprimir los primeros cinco países y almacénarlos en una variable nordic_countries, almacene Estonia y Russia en es y ru respectivamente
+
+names = ["Finland", "Sweden", "Norway", "Denmark", "Iceland", "Estonia", "Russia"]
+
+*nordic_countries, es, ru = names
+
+print(f"\n{nordic_countries}")
+print(es)
+print(f"{ru}\n")
